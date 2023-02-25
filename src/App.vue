1192819e82da27e7c76dcc586edf4c1eacaafb65
anangyoga <aharwinto@gmail.com>
2023-02-25 10:40:56 +0700

    feat: fetched & displayed data on cards

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,27 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import Card from "./components/Card.vue";
+
+const dataSurah = ref([]);
+
+const API = `https://al-quran-8d642.firebaseio.com/surat/56.json?print=pretty`;
+
+const fetchApi = async () => {
+  const res = await fetch(API);
+  const data = await res.json();
+
+  dataSurah.value = data;
+
+  return data;
+};
+
+fetchApi();
+</script>
 
 <template>
   <h1 className="text-3xl font-bold text-emerald-600 font-slab text-center py-5 border border-b-2 border-x-0 border-emerald-400">Surah Al Waqiah</h1>
+
   <div class="flex flex-col gap-y-2 max-w-3xl py-6 mx-auto divide-y-4 divide-green-100">
-    <div class="px-4 py-3">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum architecto nisi qui nostrum, dolor vel doloremque rerum totam dolorem omnis necessitatibus, nemo magnam optio quibusdam fugit? Saepe harum voluptatum ad reprehenderit.
-      Magni doloremque cumque delectus rem eum temporibus quidem, corporis dolorem iusto quo consectetur veritatis est repellendus facilis soluta accusantium odit fugiat. Perferendis exercitationem, nam, provident fugiat tenetur totam sit
-      explicabo temporibus doloribus culpa molestias, sunt iure dolores quae quis error. Nemo quod mollitia delectus saepe magnam corrupti doloremque maiores, aspernatur dicta illo dolores facilis, adipisci a nam hic. Suscipit deleniti
-      eaque soluta tenetur delectus totam autem facere excepturi fugiat?
-    </div>
-    <div class="px-4 py-3">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum architecto nisi qui nostrum, dolor vel doloremque rerum totam dolorem omnis necessitatibus, nemo magnam optio quibusdam fugit? Saepe harum voluptatum ad reprehenderit.
-      Magni doloremque cumque delectus rem eum temporibus quidem, corporis dolorem iusto quo consectetur veritatis est repellendus facilis soluta accusantium odit fugiat. Perferendis exercitationem, nam, provident fugiat tenetur totam sit
-      explicabo temporibus doloribus culpa molestias, sunt iure dolores quae quis error. Nemo quod mollitia delectus saepe magnam corrupti doloremque maiores, aspernatur dicta illo dolores facilis, adipisci a nam hic. Suscipit deleniti
-      eaque soluta tenetur delectus totam autem facere excepturi fugiat?
-    </div>
-    <div class="px-4 py-3">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum architecto nisi qui nostrum, dolor vel doloremque rerum totam dolorem omnis necessitatibus, nemo magnam optio quibusdam fugit? Saepe harum voluptatum ad reprehenderit.
-      Magni doloremque cumque delectus rem eum temporibus quidem, corporis dolorem iusto quo consectetur veritatis est repellendus facilis soluta accusantium odit fugiat. Perferendis exercitationem, nam, provident fugiat tenetur totam sit
-      explicabo temporibus doloribus culpa molestias, sunt iure dolores quae quis error. Nemo quod mollitia delectus saepe magnam corrupti doloremque maiores, aspernatur dicta illo dolores facilis, adipisci a nam hic. Suscipit deleniti
-      eaque soluta tenetur delectus totam autem facere excepturi fugiat?
-    </div>
-    <div class="px-4 py-3">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum architecto nisi qui nostrum, dolor vel doloremque rerum totam dolorem omnis necessitatibus, nemo magnam optio quibusdam fugit? Saepe harum voluptatum ad reprehenderit.
-      Magni doloremque cumque delectus rem eum temporibus quidem, corporis dolorem iusto quo consectetur veritatis est repellendus facilis soluta accusantium odit fugiat. Perferendis exercitationem, nam, provident fugiat tenetur totam sit
-      explicabo temporibus doloribus culpa molestias, sunt iure dolores quae quis error. Nemo quod mollitia delectus saepe magnam corrupti doloremque maiores, aspernatur dicta illo dolores facilis, adipisci a nam hic. Suscipit deleniti
-      eaque soluta tenetur delectus totam autem facere excepturi fugiat?
-    </div>
-    <div class="px-4 py-3">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum architecto nisi qui nostrum, dolor vel doloremque rerum totam dolorem omnis necessitatibus, nemo magnam optio quibusdam fugit? Saepe harum voluptatum ad reprehenderit.
-      Magni doloremque cumque delectus rem eum temporibus quidem, corporis dolorem iusto quo consectetur veritatis est repellendus facilis soluta accusantium odit fugiat. Perferendis exercitationem, nam, provident fugiat tenetur totam sit
-      explicabo temporibus doloribus culpa molestias, sunt iure dolores quae quis error. Nemo quod mollitia delectus saepe magnam corrupti doloremque maiores, aspernatur dicta illo dolores facilis, adipisci a nam hic. Suscipit deleniti
-      eaque soluta tenetur delectus totam autem facere excepturi fugiat?
-    </div>
-    <div class="px-4 py-3">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum architecto nisi qui nostrum, dolor vel doloremque rerum totam dolorem omnis necessitatibus, nemo magnam optio quibusdam fugit? Saepe harum voluptatum ad reprehenderit.
-      Magni doloremque cumque delectus rem eum temporibus quidem, corporis dolorem iusto quo consectetur veritatis est repellendus facilis soluta accusantium odit fugiat. Perferendis exercitationem, nam, provident fugiat tenetur totam sit
-      explicabo temporibus doloribus culpa molestias, sunt iure dolores quae quis error. Nemo quod mollitia delectus saepe magnam corrupti doloremque maiores, aspernatur dicta illo dolores facilis, adipisci a nam hic. Suscipit deleniti
-      eaque soluta tenetur delectus totam autem facere excepturi fugiat?
-    </div>
-    <div class="px-4 py-3">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum architecto nisi qui nostrum, dolor vel doloremque rerum totam dolorem omnis necessitatibus, nemo magnam optio quibusdam fugit? Saepe harum voluptatum ad reprehenderit.
-      Magni doloremque cumque delectus rem eum temporibus quidem, corporis dolorem iusto quo consectetur veritatis est repellendus facilis soluta accusantium odit fugiat. Perferendis exercitationem, nam, provident fugiat tenetur totam sit
-      explicabo temporibus doloribus culpa molestias, sunt iure dolores quae quis error. Nemo quod mollitia delectus saepe magnam corrupti doloremque maiores, aspernatur dicta illo dolores facilis, adipisci a nam hic. Suscipit deleniti
-      eaque soluta tenetur delectus totam autem facere excepturi fugiat?
-    </div>
-    <div class="px-4 py-3">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum architecto nisi qui nostrum, dolor vel doloremque rerum totam dolorem omnis necessitatibus, nemo magnam optio quibusdam fugit? Saepe harum voluptatum ad reprehenderit.
-      Magni doloremque cumque delectus rem eum temporibus quidem, corporis dolorem iusto quo consectetur veritatis est repellendus facilis soluta accusantium odit fugiat. Perferendis exercitationem, nam, provident fugiat tenetur totam sit
-      explicabo temporibus doloribus culpa molestias, sunt iure dolores quae quis error. Nemo quod mollitia delectus saepe magnam corrupti doloremque maiores, aspernatur dicta illo dolores facilis, adipisci a nam hic. Suscipit deleniti
-      eaque soluta tenetur delectus totam autem facere excepturi fugiat?
-    </div>
-    <div class="px-4 py-3">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum architecto nisi qui nostrum, dolor vel doloremque rerum totam dolorem omnis necessitatibus, nemo magnam optio quibusdam fugit? Saepe harum voluptatum ad reprehenderit.
-      Magni doloremque cumque delectus rem eum temporibus quidem, corporis dolorem iusto quo consectetur veritatis est repellendus facilis soluta accusantium odit fugiat. Perferendis exercitationem, nam, provident fugiat tenetur totam sit
-      explicabo temporibus doloribus culpa molestias, sunt iure dolores quae quis error. Nemo quod mollitia delectus saepe magnam corrupti doloremque maiores, aspernatur dicta illo dolores facilis, adipisci a nam hic. Suscipit deleniti
-      eaque soluta tenetur delectus totam autem facere excepturi fugiat?
-    </div>
+    <Card v-for="data in dataSurah" :data="data" :key="data.nomor" />
   </div>
 </template>
---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { defineProps } from "vue";
+
+const { data } = defineProps(["data"]);
+</script>
+
+<template>
+  <div class="flex flex-col gap-y-2 px-4 py-3">
+    <p class="p-1 bg-green-300 w-8 text-center">{{ data.nomor }}.</p>
+
+    <div class="flex justify-end text-lg">
+      <p>
+        {{ data.ar }}
+      </p>
+    </div>
+
+    <p class="font-slab">{{ data.id }}</p>
+  </div>
+</template>
